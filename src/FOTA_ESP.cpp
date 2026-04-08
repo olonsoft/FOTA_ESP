98e50383a5fe9176add8b6b789174e78db3d6cdb
Dimitris Paterakis <dimitris19@gmail.com>
2026-04-08 17:37:31 +0300

    Updated StaticJsonDocument to JsonDocument

--- src/FOTA_ESP.cpp
+++ src/FOTA_ESP.cpp
@@ -13,16 +13,16 @@
 #define FOTA_STR "[FOTA] "
 #endif
 
-#ifdef ESP8266    
+#ifdef ESP8266
   #define ESP_UPD ESPhttpUpdate
-#elif defined(ESP32)    
+#elif defined(ESP32)
   #define ESP_UPD httpUpdate
-#endif    
+#endif
 
 char buffer[200];
 
 /*
-char *strFOTA[] PROGMEM = { 
+char *strFOTA[] PROGMEM = {
   "Start",
   "No Response Errror",
   "Parse Error",
@@ -87,10 +87,10 @@ String FOTAClientClass::_getPayload() {
     //Serial.println(f);
     // please follow the exact order as the next two lines. Exception occurs otherwise
     WiFiClient client;
-    // the next, must be declared after WiFiClient for correct destruction order, 
+    // the next, must be declared after WiFiClient for correct destruction order,
     // because used by http.begin(client,...)
-    HTTPClient http; 
-    http.begin(client, (char *) f.c_str());    
+    HTTPClient http;
+    http.begin(client, (char *) f.c_str());
     http.useHTTP10(true);
     http.setReuse(false);
     http.setTimeout(HTTP_TIMEOUT);
@@ -129,12 +129,12 @@ bool FOTAClientClass::checkUpdates() {
     _newFileSystem = false;
 
     String payload = _getPayload();
-    if (payload.length() == 0) {        
+    if (payload.length() == 0) {
         snprintf_P(buffer, sizeof(buffer), PSTR("%sNo Response Error.\n"), FOTA_STR);
         _doCallback(FOTA_NO_RESPONSE_ERROR, buffer);
         return false;
     }
-    StaticJsonDocument<500> doc;
+    JsonDocument doc;
     DeserializationError err = deserializeJson(doc, payload);
     if (err) {
         snprintf_P(buffer, sizeof(buffer), PSTR("%sDeserializeJson Error.\n"), FOTA_STR);
@@ -154,19 +154,19 @@ bool FOTAClientClass::checkUpdates() {
 
     _newFirmware   = (_newFwVersion != _currentFwVersion);
     _newFileSystem = (_newFsVersion   != _currentFsVersion);
-    if (!_newFirmware && !_newFileSystem) {   
-       snprintf_P(buffer, sizeof(buffer), PSTR("%sUpToDate.\n"), FOTA_STR);      
+    if (!_newFirmware && !_newFileSystem) {
+       snprintf_P(buffer, sizeof(buffer), PSTR("%sUpToDate.\n"), FOTA_STR);
         _doCallback(FOTA_UPTODATE, buffer);
         return false;
     }
-    
-    if (_newFirmware) {        
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sNew firmware found: %s\n"), FOTA_STR, _newFwVersion.c_str());      
+
+    if (_newFirmware) {
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sNew firmware found: %s\n"), FOTA_STR, _newFwVersion.c_str());
         _doCallback(FOTA_NEW_FIRMWARE_FOUND, buffer);
-    } 
-    if (_newFileSystem) {        
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sNew filesystem found: %s \n"), FOTA_STR, _newFsVersion.c_str());      
-        _doCallback(FOTA_NEW_FILESYSTEM_FOUND, buffer); 
+    }
+    if (_newFileSystem) {
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sNew filesystem found: %s \n"), FOTA_STR, _newFsVersion.c_str());
+        _doCallback(FOTA_NEW_FILESYSTEM_FOUND, buffer);
     }
     return true;
 
@@ -182,27 +182,27 @@ bool FOTAClientClass::newFileSystemFound() {
 
 bool FOTAClientClass::updateFirmware(String FOTA_bin_url) {
     //Serial.println(__func__);
-    snprintf_P(buffer, sizeof(buffer), PSTR("%sUpdating firmware to: %s\n"), 
+    snprintf_P(buffer, sizeof(buffer), PSTR("%sUpdating firmware to: %s\n"),
                FOTA_STR, FOTA_bin_url.c_str());
     _doCallback(FOTA_UPDATING_FIRMWARE, buffer);
     bool result = false;
- 
-    ESP_UPD.rebootOnUpdate(false); 
-    //Serial.println(FOTA_bin_url);    
-    WiFiClient client;  
-    
+
+    ESP_UPD.rebootOnUpdate(false);
+    //Serial.println(FOTA_bin_url);
+    WiFiClient client;
+
     t_httpUpdate_return ret = ESP_UPD.update(client, FOTA_bin_url);
 
     if (ret == HTTP_UPDATE_FAILED) {
         result = false;
         _errorNumber = ESP_UPD.getLastError();
         _errorString = ESP_UPD.getLastErrorString();
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sFirmware update error: %s.\n"), FOTA_STR, _errorString.c_str());        
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sFirmware update error: %s.\n"), FOTA_STR, _errorString.c_str());
         _doCallback(FOTA_FIRMWARE_UPDATE_ERROR, buffer);
 
-    } else if (ret == HTTP_UPDATE_OK) {        
+    } else if (ret == HTTP_UPDATE_OK) {
         result = true;
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sFirmware uploaded.\n"), FOTA_STR);        
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sFirmware uploaded.\n"), FOTA_STR);
         _doCallback(FOTA_FIRMWARE_UPLOADED, buffer);
     }
     return result;
@@ -210,29 +210,29 @@ bool FOTAClientClass::updateFirmware(String FOTA_bin_url) {
 
 bool FOTAClientClass::updateFileSystem(String FOTA_fs_url) {
     //Serial.println(__func__);
-    snprintf_P(buffer, sizeof(buffer), PSTR("%sUpdating filesystem to: %s\n"), 
-               FOTA_STR, FOTA_fs_url.c_str());        
+    snprintf_P(buffer, sizeof(buffer), PSTR("%sUpdating filesystem to: %s\n"),
+               FOTA_STR, FOTA_fs_url.c_str());
     _doCallback(FOTA_UPDATING_FILESYSTEM, buffer);
     bool result = false;
-    ESP_UPD.rebootOnUpdate(false); 
-    //Serial.println(FOTA_fs_url);     
-    ESP_FS.end(); // unmound file system 
+    ESP_UPD.rebootOnUpdate(false);
+    //Serial.println(FOTA_fs_url);
+    ESP_FS.end(); // unmound file system
     WiFiClient client;
-#ifdef ESP32    
+#ifdef ESP32
     t_httpUpdate_return ret = ESP_UPD.updateSpiffs(client, FOTA_fs_url);
-#elif defined(ESP8266)    
+#elif defined(ESP8266)
     t_httpUpdate_return ret = ESP_UPD.updateFS(client, FOTA_fs_url);
 #endif
     if (ret == HTTP_UPDATE_FAILED) {
         result = false;
         _errorNumber = ESP_UPD.getLastError();
         _errorString = ESP_UPD.getLastErrorString();
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sFilesystem update error: %s.\n"), FOTA_STR, _errorString.c_str());            
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sFilesystem update error: %s.\n"), FOTA_STR, _errorString.c_str());
         _doCallback(FOTA_FILESYSTEM_UPDATE_ERROR, buffer);
 
-    } else if (ret == HTTP_UPDATE_OK) {        
+    } else if (ret == HTTP_UPDATE_OK) {
         result = true;
-        snprintf_P(buffer, sizeof(buffer), PSTR("%sFilesystem uploaded.\n"), FOTA_STR);    
+        snprintf_P(buffer, sizeof(buffer), PSTR("%sFilesystem uploaded.\n"), FOTA_STR);
         _doCallback(FOTA_FILESYSTEM_UPLOADED, buffer);
     }
     return result;
@@ -240,12 +240,12 @@ bool FOTAClientClass::updateFileSystem(String FOTA_fs_url) {
 
 void FOTAClientClass::checkAndUpdateFOTA(bool reboot) {
     //Serial.println(__func__);
-    snprintf_P(buffer, sizeof(buffer), 
-        PSTR("%sStart.\n\tFOTA url: %s\n\tJSON_file: %s\n\tCurrent FW version: %s\n\tCurrent FS version: %s\n"), 
-        FOTA_STR, 
-        _FOTA_url.c_str(), 
-        _jsonFile.c_str(), 
-        _currentFwVersion.c_str(), 
+    snprintf_P(buffer, sizeof(buffer),
+        PSTR("%sStart.\n\tFOTA url: %s\n\tJSON_file: %s\n\tCurrent FW version: %s\n\tCurrent FS version: %s\n"),
+        FOTA_STR,
+        _FOTA_url.c_str(),
+        _jsonFile.c_str(),
+        _currentFwVersion.c_str(),
         _currentFsVersion.c_str());
     _doCallback(FOTA_START, buffer);
     bool error = false;
@@ -260,11 +260,11 @@ void FOTAClientClass::checkAndUpdateFOTA(bool reboot) {
             if (!error) updates++;
         }
     }
-    snprintf_P(buffer, sizeof(buffer), PSTR("%sEnd.\n"), FOTA_STR);    
+    snprintf_P(buffer, sizeof(buffer), PSTR("%sEnd.\n"), FOTA_STR);
     _doCallback(FOTA_END, buffer);
     if (!error && (updates > 0)) {
         if (reboot) {
-            snprintf_P(buffer, sizeof(buffer), PSTR("%sRebooting...\n"), FOTA_STR);    
+            snprintf_P(buffer, sizeof(buffer), PSTR("%sRebooting...\n"), FOTA_STR);
             _doCallback(FOTA_RESET, buffer);
             ESP.restart();
             delay(1000);
